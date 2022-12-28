1ee17af738f9d9781ba6bc92bb25780740da5025
Ashton <asch8216@gmail.com>
2022-12-28 07:35:40 -0500

    update filter

--- tests/02_2DArray/2DArray.js
+++ tests/02_2DArray/2DArray.js
@@ -1,6 +1,6 @@
 // looks good
 function twoDArray(arr) {
-  // one comment when using reduce is that you are setting the accumulator each time so don't need += just +
+  // one comment when using reduce is that you are setting the accumulator each time so don't need += just + works
   const total = arr.flat().reduce((acc, ini) => acc + ini, 0);
   return total;
 }
